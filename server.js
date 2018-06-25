05349feed4484444d1e31d1ccd844bb06ea6470f
Ecksi <jimmy.dechant@gmail.com>
2018-06-25 16:07:05 -0600

    Setup Html boilerplate

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-
+app.locals.title = 'Palette Picker';
 app.use(bodyParser.json());
 app.use(express.static('public'));
-
-app.get('/', (request, response) => {
-  response.send('PALLLLETTTTTTTEEE PICKERRRRRRRR');
-});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
