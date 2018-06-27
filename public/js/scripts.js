9e9687a0a81356012e6e09fb6e20fb1d38eaf474
Ecksi <jimmy.dechant@gmail.com>
2018-06-27 08:23:53 -0600

    Add initial basic javascript
    Add event listeners for all clickables
    Add method to generate random color palette

---
+++ public/js/scripts.js
@@ -0,0 +1,29 @@
+$('.generate-palette').click(() => generatePalette());
+$('.lock-icon').click(function () {$(this).toggleClass('locked')});
+$('.save-palette-button').click(() => {
+  event.preventDefault();
+  console.log('save palette');
+  // add each card to array save values to project object
+})
+$('.save-project-button').click(() => {
+  event.preventDefault();
+  console.log('save project');
+  // add project w/ palette to endpoint(post)
+})
+$('.delete-saved-palette').click(() => console.log('delete button'));
+
+const generateRandomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16).toUpperCase();
+
+const generatePalette = () => {
+  $(".color-card").each(function() {
+    const generatedColor = generateRandomColor();
+    const isLocked = $(this).children()[1].classList[1] === 'locked';
+    
+    if (!isLocked) {
+      $(this).children()[0].innerText = generatedColor;
+      $(this).css({ 'background-color': generatedColor });
+    }
+  });
+};
+
+generatePalette();
