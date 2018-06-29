92a956153de58400a211af0c64e840f210a8be7d
Ecksi <jimmy.dechant@gmail.com>
2018-06-29 12:52:21 -0600

    Add describe blocks for API route tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -2,8 +2,6 @@ const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
-// const configuration = require('../knexfile')[development];
-// const knex = require('knex')(configuration);
 
 chai.use(chaiHttp);
 
@@ -29,42 +27,9 @@ describe('CLIENT routes', () => {
 });
 
 describe('API routes', () => {
-  beforeEach(done => knex.migrate.rollback()
-    .then(() => knex.migrate.latest()
-      .then(() => knex.seed.run()
-        .then(() => done()
-        )
-      )
-    )
-  );
+  describe('GET /api/v1/projects', () => {});
 
-  afterEach(function(done) {
-    knex.migrate.rollback()
-      .then(() => done());
-  });
-
-  describe('GET /api/v1/projects', () => {
-    it('returns an array of all projects', done => {
-      chai.request(server)
-        .get('/api/v1/projects')
-    })
-  });
-
-  describe('GET /api/v1/palettes', () => {
-    it('returns an array of all palettes', done => {
-      chai.request(server)
-        .get('/api/v1/palettes')
-        .end((error, response) => {
-          response.should.have.status(200);
-          response.should.be.json;
-          response.should.be.a('array');
-          response.body.length.should.equal(2);
-          response.body[0].should.have.property('name');
-          response.body[0].name.should.equal('Almost out');
-          done();
-        });
-    });
-  });
+  describe('GET /api/v1/palettes', () => {});
 
   describe('POST /api/v1/projects', () => {});
 
