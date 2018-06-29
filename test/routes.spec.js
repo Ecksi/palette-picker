24b895bc90bff7f5a964efc8e9e36e3ba1a2c570
Ecksi <jimmy.dechant@gmail.com>
2018-06-29 07:59:20 -0600

    Add tests for client routes

---
+++ test/routes.spec.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('CLIENT routes', () => {
+  it('should receive a response of html when we hit the root end point', done => {
+    chai.request(server)
+      .get('/')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.html;
+        done();
+      });
+  });
+
+  it('should return a 404 for a route that does not exist', done => {
+    chai.request(server)
+      .get('/trashpanda')
+      .end((error, response) => {
+        response.should.have.status(404);
+        done();
+      });
+  });
+});
+
+describe('API routes', () => {
+ 
+});
