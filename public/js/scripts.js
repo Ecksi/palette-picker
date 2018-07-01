e4cdb72ad2d63d6113be58e23cdeebb68263fd45
Ecksi <jimmy.dechant@gmail.com>
2018-07-01 11:33:50 -0600

    Convert event listeners to .on, fix toggleLock

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,20 +1,10 @@
-$('.generate-palette').click(() => generatePalette());
-$('.color-card').click(function () {
-  $(this).children()[1].classList[1] !== 'locked'
-    ? $(this).children()[1].classList.add('locked')
-    : $(this).children()[1].classList.remove('locked');
-});
-$('.save-palette-button').click(() => { savePalette();});
-$('.save-project-button').click(() => {});
-$('.delete-saved-palette').click(() => console.log('delete button'));
-
 const generateRandomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16).toUpperCase();
 
 const generatePalette = () => {
-  $(".color-card").each(function() {
+  $('.color-card').each(function() {
     const generatedColor = generateRandomColor();
     const isLocked = $(this).children()[1].classList[1] === 'locked';
-    
+
     if (!isLocked) {
       $(this).children()[0].innerText = generatedColor;
       $(this).css({ 'background-color': generatedColor });
@@ -22,6 +12,16 @@ const generatePalette = () => {
   });
 };
 
-const savePalette = () => {};
+function toggleLock() {
+  $(this).children()[1].classList[1] !== 'locked'
+    ? $(this).children()[1].classList.add('locked')
+    : $(this).children()[1].classList.remove('locked');
+}
+
+$('.generate-palette').on('click', generatePalette);
+$('.color-card').on('click', toggleLock);
+$('.save-palette-button').on('click', addPalette);
+$('.save-project-button').on('click', addProject);
+$('.split-projects').on('click', '.delete-saved-palette', deletePalette);
 
 generatePalette();
